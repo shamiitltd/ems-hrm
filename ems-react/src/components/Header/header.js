0d46ffa13e7e528297f5da52a780bcd25d5da524
Puja Hagone <66102795+poojahagone321@users.noreply.github.com>
2024-10-16 10:33:11 +0530

    User Profile

--- ems-react/src/components/Header/header.js
+++ ems-react/src/components/Header/header.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import './header.css';
-import { useNavigate } from 'react-router-dom';
+import user from '../Header/images/user.png';
+import logo from '../Header/images/Shamilogo.png';
+import userLogo from '../Header/images/profile.png'
+import setting from '../Header/images/setting.png'
+import logout from '../Header/images/logout.png'
+import help from '../Header/images/help.png'
 
+
+import { Link } from 'react-router-dom';
 
 const Header = ()=>{
-    const navigate = useNavigate();
-
-    const UserProfile = () => {
-        navigate("/UserProfile");
-      };
-    
-
-
     return(
         <>
             <div className='header-main d-flex'>
@@ -19,7 +18,8 @@ const Header = ()=>{
                     <button className="border b1 bg-white"></button><button className="border b1 bg-secondary"></button>
                 </div>
                 <div className='top2 text-center border-end'>
-                <img src='' className='fimg' /> <span>Shamiit</span>
+                <img src={logo} className='fimg' /> 
+
                 </div>
                 <div className='me-auto p-3'>
                     <button className='btn border-0'><i className="fa-solid fa-magnifying-glass"></i></button>
@@ -31,10 +31,40 @@ const Header = ()=>{
                     <div className='bg-secondary rounded-circle text-center pt-2 me-2 circle1'>
                         <a href='#'><i class="fa-regular fa-bell"></i></a>
                     </div>
-                    <div className='bg-secondary rounded-circle text-center pt-2 me-2 circle1'>
-                        <a onClick = {UserProfile}><i class="fa-solid fa-user"></i></a>
-                    </div>
+                       <img className ='user-img' src={user} alt="" /> 
                 </div>
+             <div className="sub-menu-wrap">
+                <div className="sub-menu">
+                    <div className="user-info">
+                        <img src={user} />
+                        <h4>Jonas Lord</h4>
+                        </div>
+                        <hr />
+
+                        <a href="#" className = "sub-menu-link">
+                            <img src={userLogo} />
+                            <p>Edit Profile</p>
+                            <span> > </span>
+                        </a>
+                        <a href="#" className = "sub-menu-link">
+                            <img src={setting} />
+                            <p>Setting & Privacy</p>
+                            <span>></span>
+                        </a>
+                        <a href="#" className = "sub-menu-link">
+                            <img src={help} />
+                            <p>Help and support</p>
+                            <span>></span>
+                        </a>
+                        <a href="#" className = "sub-menu-link">
+                            <img src={logout} />
+                            <p>Logout</p>
+                            <span>></span>
+                        </a>
+
+
+                        </div>
+                        </div>  
             </div>
         </>
     )
--- ems-react/src/components/UserProfile/UserProfile.js
+++ ems-react/src/components/UserProfile/UserProfile.js
@@ -1,88 +1,12 @@
-import React, { useState } from "react";
-import { Avatar, Button, TextField, Grid, Paper, Typography } from "@mui/material";
+import React from 'react'
 
-function UserProfile() {
-  const [isEditing, setIsEditing] = useState(false);
-  const [user, setUser] = useState({
-    username: "john_doe",
-    email: "john.doe@example.com",
-    role: "Employee",
-    profilePicture: "https://via.placeholder.com/150", // Placeholder for profile picture
-  });
-
-  // Toggle between Edit and Save mode
-  const handleEditClick = () => {
-    setIsEditing(!isEditing);
-  };
-
-  // Handle changes to input fields
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setUser((prevUser) => ({
-      ...prevUser,
-      [name]: value,
-    }));
-  };
-
+const UserProfile = () => {
   return (
-    <Paper elevation={3} style={{ padding: "20px", maxWidth: "600px", margin: "20px auto" }}>
-      <Grid container spacing={2} direction="column" alignItems="center">
-        <Grid item>
-          <Avatar
-            src={user.profilePicture}
-            alt="Profile Picture"
-            style={{ width: "100px", height: "100px" }}
-          />
-        </Grid>
-
-        <Grid item>
-          <Typography variant="h5">{user.username}</Typography>
-          <Typography variant="subtitle1">{user.role}</Typography>
-        </Grid>
-
-        <Grid item container spacing={2}>
-          <Grid item xs={12}>
-            <TextField
-              label="Username"
-              name="username"
-              value={user.username}
-              onChange={handleChange}
-              disabled={!isEditing}
-              fullWidth
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              label="Email"
-              name="email"
-              value={user.email}
-              onChange={handleChange}
-              disabled={!isEditing}
-              fullWidth
-            />
-          </Grid>
-
-          <Grid item xs={12}>
-            <TextField
-              label="Role"
-              name="role"
-              value={user.role}
-              onChange={handleChange}
-              disabled={!isEditing}
-              fullWidth
-            />
-          </Grid>
-        </Grid>
-
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={handleEditClick}>
-            {isEditing ? "Save" : "Edit Profile"}
-          </Button>
-        </Grid>
-      </Grid>
-    </Paper>
-  );
+    <div class = "Hero">
+       
+      
+    </div>
+  )
 }
 
-export default UserProfile;
+export default UserProfile
